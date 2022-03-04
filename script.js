ffd97dd3ad01acd307985d9d8facab9772d8e0c5
Christopher Sostre <christophersostre12@gmail.com>
2022-03-04 03:55:30 -0500

    My task is done

--- script.js
+++ script.js
@@ -1,62 +1,58 @@
-let colorSelected; 
-
+let colorSelected;
 //! this is for you
 function addR() {
     let grid = document.getElementById("grid");
     let rows = document.getElementsByTagName("tr");
     console.log(rows.length);
-    
+
     if (rows.length === 0 || rows.length >= 0) {
         let row = document.createElement("tr");
+        row.setAttribute("id", "test");
         let col = document.createElement("td");
-        col.onclick = function (){
+        col.setAttribute("id", "test1");
+        col.onclick = function () {
             this.style.backgroundColor = colorSelected;
         };
         row.appendChild(col);
         grid.appendChild(row);
     }
+
 }
 //Adds a column done by EricDittus 
 function addC() {
     //alert("Clicked Add Col")
     let cols = document.getElementsByTagName("td");
-    console.log(cols.length); 
+    console.log(cols.length);
+}
+//! this is for you
+function removeR() {
+    var el = document.getElementById("test");
+    var el2 = document.getElementById("test1");
+    el.remove();
+    el2.remove();
 }
 
-//! this is for you 
-function removeR() {
-    alert("Clicked Remove Row")
-    if (rows.length === 0 || rows.length >= 0) {
-        let row = document.createElement("tr");
-        let col = document.createElement("td");
-        col.onclick = function (){
-            this.style.backgroundColor = colorSelected;
-        };
-        row.appendChild(col);
-        grid.appendChild(row);
-    }
-}
 //Remove a column done by EricDittus 
 function removeC() {
     alert("Clicked Remove Col")
 }
 //sets global var for selected color
-function selected(){
+function selected() {
     colorSelected = document.getElementById("selectedID").value;
     console.log(colorSelected);
 }
 
 //done by others
-function fill(){
+function fill() {
     alert("Clicked Fill All")
 }
 
 //! this is for you
-function clearAll(){
+function clearAll() {
     alert("Clicked Clear All")
 }
 
 //done by others
-function fillU(){
+function fillU() {
     alert("Clicked Fill All Uncolored")
 }
