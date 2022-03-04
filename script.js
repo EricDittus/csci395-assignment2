fee951a4bc8364a8448ecf842c48bca354d6d948
Christopher Sostre <christophersostre12@gmail.com>
2022-03-03 23:15:03 -0500

    my edit

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-let colorSelected;
+let colorSelected; 
 
-//Adds a row
+//! this is for you
 function addR() {
-    //alert("Clicked Add Row")
     let grid = document.getElementById("grid");
     let rows = document.getElementsByTagName("tr");
     console.log(rows.length);
-
-    if (rows.length === 0) {
+    
+    if (rows.length === 0 || rows.length >= 0) {
         let row = document.createElement("tr");
         let col = document.createElement("td");
         col.onclick = function (){
@@ -15,21 +14,29 @@ function addR() {
         };
         row.appendChild(col);
         grid.appendChild(row);
-
     }
 }
-//Adds a column test
+//Adds a column done by EricDittus 
 function addC() {
     //alert("Clicked Add Col")
     let cols = document.getElementsByTagName("td");
-    console.log(cols.length);
+    console.log(cols.length); 
 }
 
-//Removes a row
+//! this is for you 
 function removeR() {
     alert("Clicked Remove Row")
+    if (rows.length === 0 || rows.length >= 0) {
+        let row = document.createElement("tr");
+        let col = document.createElement("td");
+        col.onclick = function (){
+            this.style.backgroundColor = colorSelected;
+        };
+        row.appendChild(col);
+        grid.appendChild(row);
+    }
 }
-//Remove a column
+//Remove a column done by EricDittus 
 function removeC() {
     alert("Clicked Remove Col")
 }
@@ -39,14 +46,17 @@ function selected(){
     console.log(colorSelected);
 }
 
+//done by others
 function fill(){
     alert("Clicked Fill All")
 }
 
+//! this is for you
 function clearAll(){
     alert("Clicked Clear All")
 }
 
+//done by others
 function fillU(){
     alert("Clicked Fill All Uncolored")
 }
